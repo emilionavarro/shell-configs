7ecc4f5b9a20154f18fa82ab917bb353520a763a
Emilio Navarro <emilio.navarro@monsanto.com>
2019-05-31 13:07:37 -0500

    Added fish config

---
+++ config.fish
@@ -0,0 +1,14 @@
+set -gx FISHCONFIG /users/gcuds/.config/fish/config.fish
+#set -gx EDITOR vim
+
+fish_vi_key_bindings
+
+set fish_user_paths 
+set PATH /Users/gcuds/.nvm/versions/node/v8.12.0/bin $PATH
+set fish_greeting " ______     ______        ______   __     ______     __  __    
+/\  ___\   /\  __ \      /\  ___\ /\ \   /\  ___\   /\ \_\ \   
+\ \ \__ \  \ \ \/\ \     \ \  __\ \ \ \  \ \___  \  \ \  __ \  
+ \ \_____\  \ \_____\     \ \_\    \ \_\  \/\_____\  \ \_\ \_\ 
+  \/_____/   \/_____/      \/_/     \/_/   \/_____/   \/_/\/_/ 
+                                                               "
+
